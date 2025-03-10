56950896a3fadf0585f54fe20988e8be398544c3
jbyen <59037547+yenyichau@users.noreply.github.com>
2025-03-10 11:21:22 +0800

    Update EsptouchSmartconfigPlugin.java

--- android/src/main/java/com/jpoh/esptouch_smartconfig/EsptouchSmartconfigPlugin.java
+++ android/src/main/java/com/jpoh/esptouch_smartconfig/EsptouchSmartconfigPlugin.java
@@ -1,83 +1,62 @@
 package com.jpoh.esptouch_smartconfig;
 
-import androidx.annotation.NonNull;
-
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.app.Activity;
-import android.location.LocationManager;
+import android.util.Log;
 
-import androidx.core.content.ContextCompat;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.BinaryMessenger;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.EventChannel;
-import io.flutter.plugin.common.EventChannel.EventSink;
+import io.flutter.plugin.common.MethodChannel;
 
-import com.espressif.iot.esptouch.EsptouchTask;
-import com.espressif.iot.esptouch.IEsptouchListener;
-import com.espressif.iot.esptouch.IEsptouchResult;
-import com.espressif.iot.esptouch.IEsptouchTask;
-import com.espressif.iot.esptouch.task.__IEsptouchTask;
-import com.espressif.iot.esptouch.EsptouchTask;
-import com.espressif.iot.esptouch.IEsptouchResult;
-import com.espressif.iot.esptouch.IEsptouchTask;
-import com.espressif.iot.esptouch.util.ByteUtil;
-import com.espressif.iot.esptouch.util.TouchNetUtil;
-
-
-/**
- * EsptouchSmartconfigPlugin
- */
 public class EsptouchSmartconfigPlugin implements FlutterPlugin {
+    private static final String TAG = "EsptouchSmartconfig";
+    private static final String EVENT_CHANNEL_NAME = "esptouch_smartconfig/result";
+    private static final String METHOD_CHANNEL_NAME = "esptouch_smartconfig";
+
     private EventChannel eventChannel;
     private MethodChannel methodChannel;
-    private static final String TAG = "esptouch_smartconfig";
-    private static final String TAG2 = "esptouch_smartconfig/result";
+    private FlutterEventChannelHandler eventChannelHandler;
 
     @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        setupChannels(flutterPluginBinding.getBinaryMessenger(), flutterPluginBinding.getApplicationContext());
-
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        Log.d(TAG, "Plugin attached to engine");
+        setupChannels(binding.getBinaryMessenger(), binding.getApplicationContext());
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        methodChannel.setMethodCallHandler(null);
-        methodChannel = null;
+        Log.d(TAG, "Plugin detached from engine");
+
+        if (methodChannel != null) {
+            methodChannel.setMethodCallHandler(null);
+            methodChannel = null;
+        }
+
+        if (eventChannel != null) {
+            eventChannel.setStreamHandler(null);
+            eventChannel = null;
+        }
+
+        eventChannelHandler = null;
     }
 
     private void setupChannels(BinaryMessenger messenger, Context context) {
-        methodChannel = new MethodChannel(messenger, TAG);
-        eventChannel = new EventChannel(messenger,TAG2);
-        FlutterEventChannelHandler flutterEventChannelHandler =
-                new FlutterEventChannelHandler(context, eventChannel);
-        final WifiManager wifiManager =
-                (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        final WifiInfoData wifiInfoFlutter = new WifiInfoData(wifiManager, context);
+        methodChannel = new MethodChannel(messenger, METHOD_CHANNEL_NAME);
+        eventChannel = new EventChannel(messenger, EVENT_CHANNEL_NAME);
 
-        final FlutterMethodChannelHandeler methodChannelHandler =
-                new FlutterMethodChannelHandeler(wifiInfoFlutter);
+        // Initialize event channel handler
+        eventChannelHandler = new FlutterEventChannelHandler(context, eventChannel);
 
+        // Initialize WifiManager
+        WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfoData wifiInfoFlutter = new WifiInfoData(wifiManager, context);
+
+        // Initialize method channel handler
+        FlutterMethodChannelHandler methodChannelHandler = new FlutterMethodChannelHandler(wifiInfoFlutter);
         methodChannel.setMethodCallHandler(methodChannelHandler);
     }
 }
-
-
-
