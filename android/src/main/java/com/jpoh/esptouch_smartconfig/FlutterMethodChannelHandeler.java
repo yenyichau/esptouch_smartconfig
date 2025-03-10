5dfc4747b0bfbfb78b9855ef955c506b3e323be5
jbyen <59037547+yenyichau@users.noreply.github.com>
2025-03-10 11:15:48 +0800

    Update FlutterMethodChannelHandeler.java

--- android/src/main/java/com/jpoh/esptouch_smartconfig/FlutterMethodChannelHandeler.java
+++ android/src/main/java/com/jpoh/esptouch_smartconfig/FlutterMethodChannelHandeler.java
@@ -1,60 +1,34 @@
 package com.jpoh.esptouch_smartconfig;
 
 import androidx.annotation.NonNull;
-
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.util.Log;
 import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
-import android.location.LocationManager;
-
-import androidx.core.content.ContextCompat;
+import android.util.Log;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-import com.espressif.iot.esptouch.EsptouchTask;
-import com.espressif.iot.esptouch.IEsptouchListener;
-import com.espressif.iot.esptouch.IEsptouchResult;
-import com.espressif.iot.esptouch.IEsptouchTask;
-import com.espressif.iot.esptouch.task.__IEsptouchTask;
-import com.espressif.iot.esptouch.EsptouchTask;
-import com.espressif.iot.esptouch.IEsptouchResult;
-import com.espressif.iot.esptouch.IEsptouchTask;
-import com.espressif.iot.esptouch.util.ByteUtil;
-import com.espressif.iot.esptouch.util.TouchNetUtil;
+/**
+ * Handles method calls from Flutter via MethodChannel
+ */
+class FlutterMethodChannelHandler implements MethodCallHandler {
+    private static final String TAG = "esptouch_smartconfig";
+    private final WifiInfoData wifiInfoFlutter;
 
- class FlutterMethodChannelHandeler implements MethodChannel.MethodCallHandler {
-    private WifiInfoData wifiInfoFlutter;
-     private static final String TAG = "esptouch_smartconfig";
-
-    FlutterMethodChannelHandeler(WifiInfoData wifiInfoFlutter) {
-        assert (wifiInfoFlutter != null);
+    FlutterMethodChannelHandler(WifiInfoData wifiInfoFlutter) {
         this.wifiInfoFlutter = wifiInfoFlutter;
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        switch (call.method) {
-            case "getWifiData":
-                result.success(wifiInfoFlutter.getWifiData());
-                break;
-            default:
-                result.notImplemented();
-                break;
+        if ("getWifiData".equals(call.method)) {
+            result.success(wifiInfoFlutter.getWifiData());
+        } else {
+            result.notImplemented();
         }
     }
 }
